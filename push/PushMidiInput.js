011da0f7ffd4382e05dbd0127de1d45c9594280c
teotigraphix <mschmalle@teotigraphix.com>
2014-07-16 12:34:12 -0400

    Refactored Push specific MidiInput into PushMidiInput. - This refactor allows the midi package to be abstract.

---
+++ push/PushMidiInput.js
@@ -0,0 +1,18 @@
+// Written by Jürgen Moßgraber - mossgrabers.de
+//            Michael Schmalle - teotigraphix.com
+// (c) 2014
+// Licensed under GPLv3 - http://www.gnu.org/licenses/gpl.html
+
+function PushMidiInput ()
+{
+    MidiInput.call (this);
+}
+
+PushMidiInput.prototype = new MidiInput();
+
+PushMidiInput.prototype.createNoteInput = function ()
+{
+    var noteInput = this.port.createNoteInput ("Ableton Push", "80????", "90????", "E0????", "B040??" /* Sustainpedal */);
+    noteInput.setShouldConsumeEvents (false);
+    return noteInput;
+};
