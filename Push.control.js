58f7f08a4abc01679a10e79a96744bd1ee9db45a
Jürgen Moßgraber <juergen.mossgraber@googlemail.com>
2014-08-18 20:58:41 +0200

    Formatting.

--- Push.control.js
+++ Push.control.js
@@ -16,9 +16,9 @@ var controller = null;
 
 host.defineController ("Ableton", "Push", "3.2", "D69AFBF0-B71E-11E3-A5E2-0800200C9A66");
 host.defineMidiPorts (1, 1);
-host.platformIsWindows() && host.addDeviceNameBasedDiscoveryPair (["MIDIIN2 (Ableton Push)"], ["MIDIOUT2 (Ableton Push)"]);
-host.platformIsLinux() && host.addDeviceNameBasedDiscoveryPair (["Ableton Push MIDI 2"], ["Ableton Push MIDI 2"]);
-host.platformIsMac() && host.addDeviceNameBasedDiscoveryPair (["Ableton Push User Port"], ["Ableton Push User Port"]);
+host.platformIsWindows () && host.addDeviceNameBasedDiscoveryPair (["MIDIIN2 (Ableton Push)"], ["MIDIOUT2 (Ableton Push)"]);
+host.platformIsLinux () && host.addDeviceNameBasedDiscoveryPair (["Ableton Push MIDI 2"], ["Ableton Push MIDI 2"]);
+host.platformIsMac () && host.addDeviceNameBasedDiscoveryPair (["Ableton Push User Port"], ["Ableton Push User Port"]);
 
 function init ()
 {
